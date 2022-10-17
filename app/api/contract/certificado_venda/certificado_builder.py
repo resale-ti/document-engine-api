1da88533bdaafbcefc22a657b36ac22f4bf46a25
Mariana Cardoso <mariana.cardoso@resale.com.br>
2022-10-17 16:00:17 -0300

    feat(certificado_build): create build certificado_venda:#certificado_venda

--- app/api/contract/certificado_venda/certificado_builder.py
+++ app/api/contract/certificado_venda/certificado_builder.py
@@ -1,8 +1,13 @@
+from operator import attrgetter
 from api.contract.contract_builder_base import ContractBuilderBase
 from api.common.repositories.wallet_repository import WalletRepository
 from api.common.repositories.property_repository import PropertyRepository
 from api.contract.certificado_venda.certificado_factory import CertificadoDocumentsFactory
+from app.api.common.repositories.document_repository import DocumentRepository
+from app.api.common.repositories.schedules_repository import SchedulesRepository
 from app.api.contract.certificado_venda.certificado_library import CertificadoVendaLibrary
+from app.api.contract.certificado_venda.certificado_facade import CertificadoVendaFacade
+from app.api.contract.regulamento_concorrencia.regulamento_factory import RegulamentoDocumentsFactory
 
 class CertificadoVendaBuilder(ContractBuilderBase):
 
@@ -19,9 +24,11 @@ class CertificadoVendaBuilder(ContractBuilderBase):
 
     def build(self) -> None:
         wallet, property, key = self.generate_property_sale_certificate()
-
-        data = self.__get_data()
-
+        
+        data = self.__get_data(wallet, property)
+        
+        documents_objects = self.__get_documents_objects_list(data)
+    
     def generate_property_sale_certificate(self):
 
         wallet = WalletRepository().get_wallet_details(self.wallet_id)
@@ -42,6 +49,38 @@ class CertificadoVendaBuilder(ContractBuilderBase):
 
     def __get_documents_objects_list(self, data):
         certificado_venda_factory = CertificadoDocumentsFactory(
-        ).get_instance(self.property_id, data)
+        ).get_instance(self.wallet_id, data)
+        
+        regulamento = RegulamentoDocumentsFactory().get_instance(data.regulamento_url)
+        
+        certificado_venda_logs = "certificadovendalog"
 
-        return certificado_venda_factory
+        return certificado_venda_factory, regulamento, certificado_venda_logs
+    
+    
+    def __get_data(self, wallet, property):
+        
+        schedule = SchedulesRepository.get_cronograma_carteira(self.wallet_id)
+        
+        regulamento_db = DocumentRepository.get_wallet_regulamento(self.wallet_id)
+        
+        for r in regulamento_db:
+            if r == 'approved' or r == 'pending':
+                abled_regulamento = r.documento_status
+                
+        last_regulamento = sorted(abled_regulamento, key=attrgetter('data_criacao'))
+        
+        # logs = 
+        
+        # sale_certificate_number = 
+        
+        certificado_venda_facade = CertificadoVendaFacade(
+            wallet,
+            schedule,
+            property,
+            last_regulamento
+            # logs,
+            # sale_certificate_number            
+        )
+        
+        return certificado_venda_facade.parse()
--- app/api/contract/certificado_venda/certificado_facade.py
+++ app/api/contract/certificado_venda/certificado_facade.py
@@ -1,7 +1,44 @@
 import os
+from tkinter import HORIZONTAL
 from api.contract.contract_builder_interface import ContractFacadeInterface
 
 class CertificadoVendaFacade(ContractFacadeInterface):
     
-      def __init__(self):
-          pass
+    def __init__(self, wallet, schedule, property, regulamento, logs, sale_certificate_number):
+        self.wallet = wallet
+        self.schedule  = schedule
+        self.property = property
+        self.regulamento = regulamento
+        self.logs = logs
+        self.sale_certificate_number = sale_certificate_number
+        
+    def parse(self)-> dict:
+        base_data = self.__get_base_data()
+        logs = self.__get_logs()
+        data = base_data + logs
+        
+        return data
+    
+    def __get_base_data(self):
+        data_final = "datafinal"
+        hora_final = 'horafinal'
+        
+        return {
+            'regulamento_url' : self.__get_regulamento_url(),
+            'CEV' : self.sale_certificate_number,
+            'MPR' : self.wallet.disputa_id,
+            'CRONOGRAMA' : self.schedule.nome,
+            'DATA_CONCORRENCIA' : data_final,
+            'HORA_CONCORRENCIA' : hora_final
+        }
+    
+    def __get_logs(self):
+        return ''
+    
+    def __get_regulamento_url(self):
+        regulamento = self.regulamento
+        
+        if len(regulamento) != 0:
+            f'configitemurl{regulamento.revisao_documento_id}'
+            
+        return ''
