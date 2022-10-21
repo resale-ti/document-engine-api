d6a629afae735e26b646277ef96b510f33b83405
Mariana Cardoso <mariana.cardoso@resale.com.br>
2022-10-21 10:31:55 -0300

    feat(regulamento_factory): create layers:#LIGA-2768

--- app/api/contract/regulamento_concorrencia/regulamento_factory.py
+++ app/api/contract/regulamento_concorrencia/regulamento_factory.py
@@ -1,4 +1,4 @@
-from api.contract.regulamento_concorrencia.regulamento_layers import MLP002Capa, MLP002Miolo, MLP002Rodape
+from api.contract.regulamento_concorrencia.regulamento_layers import MLP002Capa, MLP002Miolo, MLP002Rodape, MLPVA001Capa, MLPVA001Miolo, MLPVA001Rodape
 
 class RegulamentoDocumentsFactory:
 
@@ -7,5 +7,6 @@ class RegulamentoDocumentsFactory:
 
         if regulamento_type == "MLP_002":
             return [MLP002Capa(wallet_id, data), MLP002Miolo(wallet_id, data), MLP002Rodape(wallet_id, data)]
-
+        elif regulamento_type == "MLPVA_001":
+            return [MLPVA001Capa(wallet_id, data), MLPVA001Miolo(wallet_id, data), MLPVA001Rodape(wallet_id, data)]
         return ""
