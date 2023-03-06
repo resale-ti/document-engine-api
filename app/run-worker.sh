b1607b57d505fc1e0526c7b0ca7898217826fc1a
Matheus Rocha <matheus.rocha@resale.com.br>
2023-03-06 18:06:48 -0300

    added .buildspec.yml

--- app/run-worker.sh
+++ app/run-worker.sh
@@ -1,1 +1,1 @@
-watchmedo auto-restart --directory=/app --pattern=*.py --recursive -- celery -A api.tasks worker -l info -Q document-engine-queue -c 1 --without-mingle
+watchmedo auto-restart --directory=/app --pattern=*.py --recursive -- celery -A api.tasks worker -l info -Q document-engine-queue -c 1 --without-mingle -n workerDocument.%h
